f93a1ecf87ff1238cbc600d0ccccd3675a78c056
Filtox <pierre-du-12@hotmail.fr>
2022-12-01 20:51:18 +0100

    Update wordpress_2.sh

--- Scaleway/wordpress_2.sh
+++ Scaleway/wordpress_2.sh
@@ -6,3 +6,9 @@ sudo apt install nfs-common -y
 sudo mkdir /var/wwww
 sudo mkdir /var/wwww/html
 sudo mount 51.15.100.73:/mnt/nfs_share /var/www/html
+sudo touch /etc/rc.local
+sudo echo "#!/bin/bash -e" >> /etc/rc.local
+sudo echo "sudo mount 51.15.100.73:/mnt/nfs_share /var/www/html" >> /etc/rc.local
+sudo echo "exit 0" >> /etc/rc.local
+chown root:root /etc/rc.local
+chmod 700 /etc/rc.local
