d07f94eaf9c5ba2c6e8386f582ba9c8aeed894ad
Antoine Zachariades <antoine.zachariades@gtd.eu>
2022-12-01 12:06:45 +0100

    Modification fichier de script instance azuer

--- Commandes_Azure_CLI.sh
+++ Commandes_Azure_CLI.sh
@@ -12,7 +12,7 @@ az network vnet create --name vnet_02 --resource-group Projet_Cloud01
 
 # Création Virtual machine - User : usercloud MDP : ynovtoulouse31!
 echo "Création de l'Instance Wordpress 2"
-az vm create --name instance_02 --resource-group Projet_Cloud01 --location francecentral --image UbuntuLTS --size Standard_B1ls --authentication-type password --admin-username usercloud --admin-password ynovtoulouse31! --vnet-name vnet_02 --public-ip-address public_ip_02 
+az vm create --name instance_02 --resource-group Projet_Cloud01 --location francecentral --image UbuntuLTS --size Standard_B1ls --authentication-type password --admin-username usercloud --admin-password ynovtoulouse31! --vnet-name vnet_02 --public-ip-address public_ip_02 --custom-data wordpress_2.sh
 
 # Ouverture des ports
 echo "Ouverture des ports 80 et 443 en plus sur l'instance"
