3c397a25b406ec154f995c9cbd262f6978f979c9
Filtox <pierre-du-12@hotmail.fr>
2022-12-01 14:18:23 +0100

    Update Commandes_Azure_CLI_creation_IP.sh

--- Commandes_Azure_CLI_creation_IP.sh
+++ Commandes_Azure_CLI_creation_IP.sh
@@ -4,6 +4,4 @@ az group create --name Projet_Cloud01 --location francecentral
 
 # Création Adresse IP publique
 echo "Création d'une Adresse IP publique"
-#az network public-ip create --name public_ip_02 --resource-group Projet_Cloud01 --location francecentral --version IPv4
-
 az network public-ip create --resource-group Projet_Cloud01 --name myStandardPublicIP --version IPv4 --sku Standard --zone 1
