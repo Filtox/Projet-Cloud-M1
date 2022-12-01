8d0663dd64cf9d0482aac9febb30f66deee879bc
Filtox <pierre-du-12@hotmail.fr>
2022-12-01 14:30:37 +0100

    Update terraform_config.tf

--- terraform_config.tf
+++ terraform_config.tf
@@ -129,7 +129,7 @@ resource "scaleway_lb_backend" "backend1" {
     # IP Instance serveur 1 Wordpress sur Scaleway - Correspond à l'IP de l'ID 06a34dcf-2927-4aee-8b8e-6e9ba4e79923
     "51.158.175.56",
     # IP Instance serveur 2 Wordpress sur Azure
-    "51.11.211.14"
+    "51.15.100.73"
   ]
 }
 
