f4dd0e2564f60faf2f9aea2129f34d4fa6c0d22d
Filtox <pierre-du-12@hotmail.fr>
2022-12-01 13:27:32 +0100

    Update wordpress.sh

--- wordpress.sh
+++ wordpress.sh
@@ -3,9 +3,9 @@ sudo apt update
 sudo apt install apache2 -y
 sudo apt install php php-mysql php-cgi php-cli php-gd -y
 wget https://wordpress.org/latest.zip
+sudo mount 51.15.100.73:/mnt/nfs_share /var/www/html
 sudo apt install unzip -y
 unzip latest.zip
-sudo mount 51.15.100.73:/mnt/nfs_share /var/www/html
 sudo cp -r wordpress/* /var/www/html/
 sudo chown -R www-data:www-data /var/www/html/
 sudo chmod -R 755 /var/www/html/
