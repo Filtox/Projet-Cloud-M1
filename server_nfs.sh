639ca5df811ec6b90210efd276bee9b61cc620aa
Filtox <pierre-du-12@hotmail.fr>
2022-12-01 11:31:59 +0100

    Update server_nfs.sh

--- server_nfs.sh
+++ server_nfs.sh
@@ -4,8 +4,8 @@ sudo apt install nfs-kernel-server -y
 sudo mkdir -p /mnt/nfs_share
 sudo chown -R nobody:nogroup /mnt/nfs_share/
 sudo chmod 777 /mnt/nfs_share/
-sudo echo "/mnt/nfs_share wp1.pierreds.studio/24(rw,sync,no_subtree_check)" >> /etc/exports
-sudo echo "/mnt/nfs_share wp2.pierreds.studio/24(rw,sync,no_subtree_check)" >> /etc/exports
+sudo echo "/mnt/nfs_share wp1.pierreds.studio(rw,sync,no_subtree_check)" >> /etc/exports
+sudo echo "/mnt/nfs_share wp2.pierreds.studio(rw,sync,no_subtree_check)" >> /etc/exports
 sudo exportfs -a
 sudo systemctl restart nfs-kernel-server
 sudo ufw allow from 51.158.175.56 to any port nfs
