2434783afda0eb991a10fddf039ef1e625c9f93a
“Valentin117” <valentin.malo@ynov.com>
2022-12-01 14:29:52 +0100

    ok

--- Commandes_Azure_CLI.sh
+++ Commandes_Azure_CLI.sh
@@ -4,7 +4,7 @@ az group create --name Projet_Cloud01 --location francecentral
 
 # Création Adresse IP publique
 echo "Création d'une Adresse IP publique"
-az network public-ip create --name public_ip_02 --resource-group Projet_Cloud01 --location francecentral --version IPv4
+az network public-ip create --name public_ip_02 --resource-group Projet_Cloud01 --location francecentral --version IPv4 --sku Stardard --zone 1
 
 # Création Réseau virtuel
 echo "Création d'un Réseau virtuel"
--- terraform_config.tf
+++ terraform_config.tf
@@ -125,7 +125,7 @@ resource "scaleway_lb_backend" "backend1" {
     # IP Instance serveur 1 Wordpress sur Scaleway - Correspond à l'IP de l'ID 06a34dcf-2927-4aee-8b8e-6e9ba4e79923
     "51.158.175.56",
     # IP Instance serveur 2 Wordpress sur Azure
-    "51.11.208.221"
+    "20.199.81.12"
   ]
 }
 
