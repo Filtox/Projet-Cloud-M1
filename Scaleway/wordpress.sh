5ecf102c09f9416bce58cf34cd0c86110695b205
Filtox <pierre-du-12@hotmail.fr>
2022-12-01 20:51:08 +0100

    Update wordpress.sh

--- Scaleway/wordpress.sh
+++ Scaleway/wordpress.sh
@@ -11,3 +11,9 @@ sudo cp -r wordpress/* /var/www/html/
 sudo chown -R www-data:www-data /var/www/html/
 sudo chmod -R 755 /var/www/html/
 sudo rm -rf /var/www/html/index.html
+sudo touch /etc/rc.local
+sudo echo "#!/bin/bash -e" >> /etc/rc.local
+sudo echo "sudo mount 51.15.100.73:/mnt/nfs_share /var/www/html" >> /etc/rc.local
+sudo echo "exit 0" >> /etc/rc.local
+chown root:root /etc/rc.local
+chmod 700 /etc/rc.local
