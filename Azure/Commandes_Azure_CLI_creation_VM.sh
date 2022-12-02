31b636d0271f79157e532e5e9ccfd530c405e0ee
Valentin117 <57916029+Valentin117@users.noreply.github.com>
2022-12-02 08:42:20 +0100

    Update Commandes_Azure_CLI_creation_VM.sh

--- Azure/Commandes_Azure_CLI_creation_VM.sh
+++ Azure/Commandes_Azure_CLI_creation_VM.sh
@@ -1,7 +1,3 @@
-# Création Réseau virtuel
-echo "Création d'un Réseau virtuel"
-az network vnet create --name vnet_02 --resource-group Projet_Cloud01
-
 # Création Virtual machine - User : usercloud MDP : ynovtoulouse31!
 echo "Création de l'Instance Wordpress 2"
 az vm create --name instance_02 --resource-group Projet_Cloud01 --location francecentral --image UbuntuLTS --size Standard_B1ls --authentication-type password --admin-username usercloud --admin-password ynovtoulouse31! --vnet-name vnet_02 --public-ip-address public_ip_02 --custom-data wordpress_2.sh
