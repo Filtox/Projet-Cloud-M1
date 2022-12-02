6f2cd474a24e03d50194d657bb565c2c8a20844f
Filtox <pierre-du-12@hotmail.fr>
2022-12-02 15:06:46 +0100

    Update terraform_config.tf

--- Scaleway/terraform_config.tf
+++ Scaleway/terraform_config.tf
@@ -22,18 +22,9 @@ provider "scaleway" {
 
 # Création Instance Serveur NFS sur Scaleway
 
-# NE JAMAIS COMMENTER LES LIGNES EN DESSOUS --
-# resource "scaleway_instance_ip" "public_ipNFS" {}
-# --
-
 resource "time_sleep" "wait_120_1" {
   create_duration = "60s"
 }
-
-# resource "scaleway_instance_volume" "dataNFS" {
-  # size_in_gb = 10
-  # type = "b_ssd"
-# }
 
 # ip_id récupére l'IP publique et l'ajoute à notre instance via l'ID
 
@@ -41,12 +32,9 @@ resource "scaleway_instance_server" "instanceNFS" {
   name   = "Instance_NFS_Projet"
   image  = "ubuntu_jammy"
   type   = "DEV1-S"
-  # ip_id  = scaleway_instance_ip.public_ipNFS.id
   ip_id  = "b81f27eb-cc57-49ef-9d9b-9bbb5c5a7eff"
   zone   = "nl-ams-1"
   depends_on = [time_sleep.wait_120_1]
-
-  # additional_volume_ids = [ scaleway_instance_volume.dataNFS.id ]
 
   user_data = {
     foo        = "bar"
@@ -56,18 +44,9 @@ resource "scaleway_instance_server" "instanceNFS" {
 
 # Création Instance Serveur 1 sur Scaleway
 
-# NE JAMAIS COMMENTER LES LIGNES EN DESSOUS --
-# resource "scaleway_instance_ip" "public_ip" {}
-# --
-
 resource "time_sleep" "wait_120" {
   create_duration = "120s"
 }
-
-# resource "scaleway_instance_volume" "data1" {
-  # size_in_gb = 10
-  # type = "b_ssd"
-# }
 
 # ip_id récupére l'IP publique et l'ajoute à notre instance via l'ID
 
@@ -75,19 +54,15 @@ resource "scaleway_instance_server" "instance1" {
   name   = "Instance_1_Projet"
   image  = "ubuntu_jammy"
   type   = "DEV1-S"
-  # ip_id  = scaleway_instance_ip.public_ip.id
   ip_id  = "06a34dcf-2927-4aee-8b8e-6e9ba4e79923"
   zone   = "nl-ams-1"
   depends_on = [time_sleep.wait_120]
-
-  # additional_volume_ids = [ scaleway_instance_volume.data1.id ]
 
   user_data = {
     foo        = "bar"
     cloud-init = file("wordpress.sh")
   }
 }
-# Ajout groupe de sécurité
 
 # Création Instance Base de données sur Scaleway
 
@@ -106,11 +81,9 @@ resource "scaleway_rdb_instance" "bdd1" {
 
 # Création Load Balancer sur Scaleway
 
-# NE JAMAIS COMMENTER LES LIGNES EN DESSOUS --
 resource "scaleway_lb_ip" "public_ip3" {
    zone = "nl-ams-1"
 }
-# --
 
 resource "scaleway_lb_backend" "backend1" {
   lb_id            = scaleway_lb.lb1.id
@@ -120,12 +93,7 @@ resource "scaleway_lb_backend" "backend1" {
 
   health_check_tcp {}
 
-  # health_check_http {
-   # uri = "/wp-admin/setup-config.php"
-  # }
-
   server_ips = [
-    # scaleway_instance_ip.public_ip.address
     # IP Instance serveur 1 Wordpress sur Scaleway - Correspond à l'IP de l'ID 06a34dcf-2927-4aee-8b8e-6e9ba4e79923
     "51.158.175.56",
     # IP Instance serveur 2 Wordpress sur Azure
@@ -144,6 +112,5 @@ resource "scaleway_lb" "lb1" {
   name  = "Load_Balancer_1_Projet"
   ip_id = scaleway_lb_ip.public_ip3.id
   zone  = scaleway_lb_ip.public_ip3.zone
-  # zone  = "nl-ams-1"
   type  = "LB-GP-L"
 }
