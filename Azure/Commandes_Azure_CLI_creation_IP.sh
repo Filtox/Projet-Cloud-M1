85c79877e39982bf8139584fcd0f7ee29cd1d0ca
Valentin117 <57916029+Valentin117@users.noreply.github.com>
2022-12-02 08:42:50 +0100

    Update Commandes_Azure_CLI_creation_IP.sh

--- Azure/Commandes_Azure_CLI_creation_IP.sh
+++ Azure/Commandes_Azure_CLI_creation_IP.sh
@@ -5,3 +5,7 @@ az group create --name Projet_Cloud01 --location francecentral
 # Création Adresse IP publique
 echo "Création d'une Adresse IP publique"
 az network public-ip create --resource-group Projet_Cloud01 --name myStandardPublicIP --version IPv4 --sku Standard --zone 1
+
+# Création Réseau virtuel
+echo "Création d'un Réseau virtuel"
+az network vnet create --name vnet_02 --resource-group Projet_Cloud01
