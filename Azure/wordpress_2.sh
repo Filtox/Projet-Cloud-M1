4a82bceaa4f81b8c1846baf7b06b9acaa4be4062
Filtox <pierre-du-12@hotmail.fr>
2022-12-02 09:56:15 +0100

    modif finies

--- Azure/wordpress_2.sh
+++ Azure/wordpress_2.sh
@@ -4,12 +4,11 @@ sudo apt install apache2 -y
 sudo apt install php php-mysql php-cgi php-cli php-gd -y
 sudo apt install nfs-common -y
 sudo mkdir -p /var/wwww/html
-sudo mount 51.15.100.73:/mnt/nfs_share /var/www/html
-sudo chmod -R 755 /var/www/html/
+sudo mount nfs.pierreds.studio:/mnt/nfs_share /var/www/html
 sudo rm -rf /var/www/html/index.html
 sudo touch /etc/rc.local
 sudo echo "#!/bin/bash -e" >> /etc/rc.local
-sudo echo "sudo mount 51.15.100.73:/mnt/nfs_share /var/www/html" >> /etc/rc.local
+sudo echo "sudo mount nfs.pierreds.studio:/mnt/nfs_share /var/www/html" >> /etc/rc.local
 sudo echo "exit 0" >> /etc/rc.local
 chown root:root /etc/rc.local
 chmod 700 /etc/rc.local
